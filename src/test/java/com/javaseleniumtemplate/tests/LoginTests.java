55d0f4314c8e339dfd2e5b6d71ee6eb8e2164249
Rafael <penharafael@gmail.com>
2022-05-22 15:56:04 -0300

    Ajuste arquitetura

--- src/test/java/com/javaseleniumtemplate/tests/LoginTests.java
+++ src/test/java/com/javaseleniumtemplate/tests/LoginTests.java
@@ -77,7 +77,8 @@ public class LoginTests extends TestBase {
         loginPage.preencherSenha(senha);
         loginPage.clicarEntrar();
 
-        Assert.assertEquals("rt", loginPage.returnMessageDeErro());
+        Assert.assertEquals("Sua conta pode estar desativada ou bloqueada ou o " +
+                "nome de usuário e a senha que você digitou não estão corretos.", loginPage.returnMessageDeErro());
     }
 
 }
